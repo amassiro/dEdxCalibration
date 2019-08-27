693a62ac5f7b0d4d5bf26f7f860f297be616d2df
amassiro <massironi.andrea@gmail.com>
2019-08-27 14:43:18 +0200

    tests

--- Plotting/script_post_smearing.sh
+++ Plotting/script_post_smearing.sh
@@ -1,7 +1,7 @@
     
-# ./draw_integral_ttree_calibrated.exe  data_calibration/1May2019/DATA-CR-2018-Hadded/SingleMuon_Run2018D_PromptReco_v2/treeProducerXtracks/tree_filtered_skimmed.root  \
-#     ../Calibration/Run2018D/scale_BPIX_pixels_run_ranges.txt   ../Calibration/Run2018D/scale_FPIX_pixels_run_ranges.txt     \
-#     data_calibration/29Apr2019/MC2018/tree_filtered_skimmed_smeared_DYJetsM50_HT100to200.root    outplots_2018data_RunD_layer1_scaled_and_smeared.root    1      1      4     65     1     0       0
+./draw_integral_ttree_calibrated.exe  data_calibration/1May2019/DATA-CR-2018-Hadded/SingleMuon_Run2018D_PromptReco_v2/treeProducerXtracks/tree_filtered_skimmed.root  \
+    ../Calibration/Run2018D/scale_BPIX_pixels_run_ranges.txt   ../Calibration/Run2018D/scale_FPIX_pixels_run_ranges.txt     \
+    data_calibration/29Apr2019/MC2018/tree_filtered_skimmed_smeared_DYJetsM50_HT100to200.root    outplots_2018data_RunD_layer1_scaled_and_smeared.root    1      1      4     65     1     0       0
     
 ./draw_integral_ttree_calibrated.exe  data_calibration/1May2019/DATA-CR-2018-Hadded/SingleMuon_Run2018D_PromptReco_v2/treeProducerXtracks/tree_filtered_skimmed.root  \
     ../Calibration/Run2018D/scale_BPIX_pixels_run_ranges.txt   ../Calibration/Run2018D/scale_FPIX_pixels_run_ranges.txt      \
